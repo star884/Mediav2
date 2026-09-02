beb16a8d289562bf08ecdcb9fdc2da8586db166c
star884 <136379187+star884@users.noreply.github.com>
2026-09-02 13:38:29 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,7 @@
 plugins {
     id("com.android.library") version "8.1.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.10" apply false
-    id("com.lagradost.cloudstream3.gradle") version "0.1.0"
-}
 
-cloudstream {
-    // Replace with your exact Render Static Site domain
-    repoUrl = "https://your-app-name.onrender.com"
+    id("com.lagradost.cloudstream3.gradle")
+        version "0.1.0"
 }
