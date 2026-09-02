86ff5cc8b770f09000da198eb98a7ee92e1c3375
star884 <136379187+star884@users.noreply.github.com>
2026-09-02 13:36:19 +0530

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,31 +3,29 @@ pluginManagement {
         google()
         mavenCentral()
         gradlePluginPortal()
-        maven { setUrl("https://jitpack.io") }
+        maven {
+            setUrl("https://jitpack.io")
+        }
     }
 }
 
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositoriesMode.set(
+        RepositoriesMode.FAIL_ON_PROJECT_REPOS
+    )
+
     repositories {
         google()
         mavenCentral()
-        maven { setUrl("https://jitpack.io") }
+        maven {
+            setUrl("https://jitpack.io")
+        }
     }
 }
 
-rootProject.name = "MyCloudstreamRepo"
+rootProject.name = "Mediav2Extensions"
 
-include(":StreamPlay")
-include(":Moviebox")
-include(":HDHUB4K")
-include(":AniDb")
-include(":Animepahe")
 include(":Cinestream")
 
-project(":StreamPlay").projectDir = file("src/StreamPlay")
-project(":Moviebox").projectDir = file("src/Moviebox")
-project(":HDHUB4K").projectDir = file("src/HDHUB4K")
-project(":AniDb").projectDir = file("src/AniDb")
-project(":Animepahe").projectDir = file("src/Animepahe")
-project(":Cinestream").projectDir = file("src/Cinestream")
+project(":Cinestream").projectDir =
+    file("src/Cinestream")
