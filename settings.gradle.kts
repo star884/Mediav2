1dfdcc5d0a3b0f49010835a34150509a04536a17
star884 <136379187+star884@users.noreply.github.com>
2026-09-02 13:01:07 +0530

    Create settings.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,34 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+        maven { setUrl("https://jitpack.io") }
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { setUrl("https://jitpack.io") }
+    }
+}
+
+rootProject.name = "MyCloudstreamRepo"
+
+// Include all 6 extensions requested from Phisherrepo & CSX
+include(":StreamPlay")
+include(":Moviebox")
+include(":HDHUB4K")
+include(":AniDb")
+include(":Animepahe")
+include(":Cinestream")
+
+project(":StreamPlay").projectDir = file("src/StreamPlay")
+project(":Moviebox").projectDir = file("src/Moviebox")
+project(":HDHUB4K").projectDir = file("src/HDHUB4K")
+project(":AniDb").projectDir = file("src/AniDb")
+project(":Animepahe").projectDir = file("src/Animepahe")
+project(":Cinestream").projectDir = file("src/Cinestream")
